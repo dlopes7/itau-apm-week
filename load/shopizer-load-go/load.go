5d39aad4c34a76b794f523f5c0de19aad7f51da2
dlopes7 <davidribeirolopes@gmail.com>
2018-11-23 12:24:43 -0200

    Update go script

--- load/shopizer-load-go/load.go
+++ load/shopizer-load-go/load.go
@@ -1,28 +1,28 @@
 package main
 
 import (
-		"fmt"
-		"io/ioutil"
 	"encoding/json"
-	"time"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
+	"time"
 )
 
-
-
+// Server points to a hostname, port where shopizer runs
 type Server struct {
 	Hostname string `json:"hostname"`
 	Port     int    `json:"port"`
 }
 
+// Endpoint represents a single shopizer endpoint
 type Endpoint struct {
 	URL    string `json:"url"`
 	Body   string `json:"body"`
 	Method string `json:"method"`
 }
 
-func readEndpoints() *[]Endpoint{
+func readEndpoints() *[]Endpoint {
 	file := "./endpoints.json"
 	raw, err := ioutil.ReadFile(file)
 	if err != nil {
@@ -37,7 +37,7 @@ func readEndpoints() *[]Endpoint{
 	return endpoints
 }
 
-func readServers(serverfile string) *[]Server{
+func readServers(serverfile string) *[]Server {
 	file := serverfile
 	raw, err := ioutil.ReadFile(file)
 	if err != nil {
@@ -52,9 +52,7 @@ func readServers(serverfile string) *[]Server{
 	return servers
 }
 
-
-func main(){
-
+func main() {
 
 	serverfile := os.Args[1]
 
@@ -62,27 +60,32 @@ func main(){
 	endpoints := *readEndpoints()
 
 	client := &http.Client{
-		Timeout: 10 * time.Second,
+		Timeout: 2 * time.Second,
 	}
-
 
 	for {
 
 		for _, server := range servers {
-			for _, endpoint := range endpoints{
-
-				url :=  fmt.Sprintf("http://%s:%d/%s", server.Hostname, server.Port, endpoint.URL)
-
-				fmt.Printf("%s:%d - %s\n", server.Hostname, server.Port, endpoint.URL)
-				req, _ := http.NewRequest(endpoint.Method, url, nil )
+			for _, endpoint := range endpoints {
 
 				time.Sleep(100 * time.Millisecond)
-				resp, err := client.Do(req)
+				url := fmt.Sprintf("http://%s:%d/%s", server.Hostname, server.Port, endpoint.URL)
+
+				fmt.Printf("%s:%d - %s\n", server.Hostname, server.Port, endpoint.URL)
+				req, err := http.NewRequest(endpoint.Method, url, nil)
 				if err != nil {
 					fmt.Printf("Error %s\n", err.Error())
-				}
-				fmt.Printf("%v\n", resp.Status)
+				} else {
 
+					resp, err := client.Do(req)
+
+					if err != nil {
+						fmt.Printf("Error %s\n", err.Error())
+					} else {
+						fmt.Printf("%v\n", resp.Status)
+					}
+
+				}
 			}
 
 		}
